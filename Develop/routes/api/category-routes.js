f5f1c962da1dbc20e5a2bd1d63d58f7a3e54d588
Ribbit <taylor.orsini@yahoo.com>
2023-05-02 23:26:11 -0700

    created final version of files, need to add verification next

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,75 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const nameData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(nameData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name,
+},{
+    where:{
+        id:req.params.id
+    }
+}).then(editCat=>{
+    if(!editCat[0]){
+        return res.status(404).json({msg:"no category with this id in database"})
+    }
+    res.json(editCat)
+}).catch(err=>{
+    console.log(err);
+    res.status(500).json({msg:"error occurred",err})
+})
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where:{
+        id:req.params.id
+    }
+}).then(delCat=>{
+    if(!delCat){
+        return res.status(404).json({msg:"no category with this id in database"})
+    }
+    res.json(delCat)
+}).catch(err=>{
+    console.log(err);
+    res.status(500).json({msg:"error occurred",err})
+})
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -3,16 +3,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try{
+    const products= await Product.findAll({
+        include:[Category],
+        include:[Tag],
+    });
+    if(products.length===0){
+        return res.status(404).json({msg:"no products in database"})
+    }
+    res.json(products)
+} catch(err){
+    console.log(err);
+    res.status(500).json({msg:"error occurred",err})
+}
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async(req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, model: Tag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -90,7 +109,19 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where:{
+        id:req.params.id
+    }
+}).then(delProduct=>{
+    if(!delProduct){
+        return res.status(404).json({msg:"no product with this id in database!"})
+    }
+    res.json(delProduct)
+}).catch(err=>{
+    console.log(err);
+    res.status(500).json({msg:"error occurred",err})
+})
 });
 
 module.exports = router;
